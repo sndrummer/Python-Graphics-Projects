7010a5877203132daa937d46d4095defec2199e7
Samuel Nuttall <sndrummer@gmail.com>
2019-03-28 17:38:51 -0600

    done with lab8

--- cs355Notebooks/lab8/Lab8-Lighting.py
+++ cs355Notebooks/lab8/Lab8-Lighting.py
@@ -8,40 +8,55 @@ import wireframe as wf
 import basicShapes as shape
 
 
-def create_array(x, y, z):
+def create_2darray(x, y, z):
+    return np.array([[x, y, z]])
+
+
+def to_1darray(vector2d):
+    v = vector2d[0]
+    x, y, z = v[0], v[1], v[2]
     return np.array([x, y, z])
 
 
-def getDiffLighting(s, mdiff, l, n):  # cdiff
-    return (s * mdiff) * np.dot(l, n)
-
-
-def getSpecLighting(s, mspec, v, r, m):
-    return (s * mspec) * (np.dot(v, r) ** m)
+def create_vector(x, y, z):
+    return np.array([[x],
+                     [y],
+                     [z]])
 
 
 def getReflectedDir(l, n):  # get r
     return (2 * np.dot(l, n) * n) - l
 
 
-def get_c_amb(s_amb, m_amb):
-    return s_amb * m_amb
+def normalize(v):
+    norm = np.linalg.norm(v)
+    if norm == 0:
+        return v
+    return v / norm
 
 
-def get_total_light(spec, diff, amb):
-    return spec + diff + amb
+def get_xrot_matrix(degrees_rotation):
+    radians = math.radians(degrees_rotation)
+    c, s = np.cos(radians), np.sin(radians)
+    return np.array([[1, 0, 0],
+                     [0, c, -s],
+                     [0, s, c]])
 
 
-class PhongLight:
-    def __init__(self, normal, lighting_dir, light_color, amb_color, view_direction, mdiff, mspec,
-                 m):
-        self.c_diff = getDiffLighting(light_color, mdiff, lighting_dir, normal)
-        self.r = getReflectedDir(lighting_dir, normal)
-        self.c_spec = getSpecLighting(light_color, mspec, view_direction, self.r, m)
-        self.c_amb = get_c_amb(amb_color, mdiff)
+def get_yrot_matrix(degrees_rotation):
+    radians = math.radians(degrees_rotation)
+    c, s = np.cos(radians), np.sin(radians)
+    return np.array([[c, 0, -s],
+                     [0, 1, 0],
+                     [s, 0, c]])
 
-    def get_total_light(self):
-        return self.c_diff + self.c_spec + self.c_amb
+
+def get_zrot_matrix(degrees_rotation):
+    radians = math.radians(degrees_rotation)
+    c, s = np.cos(radians), np.sin(radians)
+    return np.array([[c, -s, 0],
+                     [s, c, 0],
+                     [0, 0, 1]])
 
 
 class WireframeViewer(wf.WireframeGroup):
@@ -103,30 +118,20 @@ class WireframeViewer(wf.WireframeGroup):
                     # Only draw faces that face us
                     if towards_us > 0:
                         ambient_strength = 0.1
-                        diffuse_strength = 0.2
-                        specular_strength = 0.7
-
-
-
-                        #ambient = self.light_color * (ambient_strength * object_color)
+                        diffuse_strength = 0.3
+                        specular_strength = 0.6
+                        shininess = 2  # m
                         ambient = ambient_strength * self.light_color
-
-
-                        #diff = np.dot(normal, self.light_dir)
 
                         diff = max(np.dot(normal, self.light_dir), 0.0)
                         diffuse = diff * (diffuse_strength * self.light_color)
 
-                        print("diff: ", diff)
-                        #lighting_dir, normal
-                        reflect_dir  = getReflectedDir(self.light_dir, normal)
+                        reflect_dir = getReflectedDir(self.light_dir, normal)
 
-                        spec = max(np.dot(self.view_dir, reflect_dir), 0.0) ** 4
+                        spec = max(np.dot(self.view_dir, reflect_dir), 0.0) ** shininess
                         specular = specular_strength * spec * self.light_color
 
-
                         light_total = (ambient + diffuse + specular) * object_color
-                        print("light total: ", light_total)
                         pygame.draw.polygon(self.screen, light_total,
                                             [(nodes[node][0], nodes[node][1]) for node in face], 0)
 
@@ -145,7 +150,8 @@ class WireframeViewer(wf.WireframeGroup):
 
                                 pygame.draw.aaline(self.screen, object_color, (x1, y1), (x2, y2), 1)
                         else:
-                            pygame.draw.aaline(self.screen, object_color, (nodes[n1][0], nodes[n1][1]),
+                            pygame.draw.aaline(self.screen, object_color,
+                                               (nodes[n1][0], nodes[n1][1]),
                                                (nodes[n2][0], nodes[n2][1]), 1)
 
             if self.displayNodes:
@@ -159,16 +165,49 @@ class WireframeViewer(wf.WireframeGroup):
         # Your code here
         if key == pygame.K_a:
             print("a is pressed")
+
+            x_val = self.light_dir[0]
+            x_val -= .25
+            self.light_dir[0] = x_val
+            self.light_dir = normalize(self.light_dir)
+
         if key == pygame.K_d:
             print("d is pressed")
+            x_val = self.light_dir[0]
+            x_val += .25
+            self.light_dir[0] = x_val
+            self.light_dir = normalize(self.light_dir)
+
         if key == pygame.K_w:
             print("w is pressed")
+            y_val = self.light_dir[1]
+            y_val -= .25
+            self.light_dir[1] = y_val
+            self.light_dir = normalize(self.light_dir)
+
         if key == pygame.K_s:
             print("s is pressed")
+            y_val = self.light_dir[1]
+            y_val += .25
+            self.light_dir[1] = y_val
+            self.light_dir = normalize(self.light_dir)
+
         if key == pygame.K_q:
             print("q is pressed")
+            v = create_2darray(self.light_dir[0], self.light_dir[1], self.light_dir[2])
+            z_rot = get_zrot_matrix(10)
+            rotated = np.matmul(v, z_rot)
+            self.light_dir = to_1darray(rotated)
+            self.light_dir = normalize(self.light_dir)
+
         if key == pygame.K_e:
             print("e is pressed")
+            print("q is pressed")
+            v = create_2darray(self.light_dir[0], self.light_dir[1], self.light_dir[2])
+            z_rot = get_zrot_matrix(-10)
+            rotated = np.matmul(v, z_rot)
+            self.light_dir = to_1darray(rotated)
+            self.light_dir = normalize(self.light_dir)
 
         return
 
@@ -210,20 +249,3 @@ for i in range(int(resolution / 4)):
 
 viewer.displayEdges = False
 viewer.run()
-
-# ambient = self.light_color * (m_ambient * colour)
-#                         s_amb = self.light_color
-#                         m_amb = m_ambient * colour
-#
-#                         m_diff = m_amb  # surface diffuse reflectance coefficients
-#
-#                         m_spec = create_array(0.9, 0.9, 0.9)  # surface specular reflectance coefficients
-#                         m = 4
-#
-#                         s = self.light_color * (m_direct * colour)
-#                         print("s: ", s)
-#                         print("light color: ", self.light_color)
-#                         diff = getDiffLighting(s, m_diff, self.light_vector, normal)  # s mdiff l n
-#                         r = getReflectedDir(self.light_vector, normal)
-#                         #print("r: ", r)
-#                         spec = getSpecLighting(s, m_spec, self.view_vector, r, m)
